a8443a4ab4e4b206b0da9ae438e5a701e57c8313
Kishore-kumar14 <kishorekumarsrini2006@gmail.com>
2025-12-08 11:19:33 +0530

    Create program.java

---
+++ program.java
@@ -0,0 +1,13 @@
+public class SimpleMultiplication {
+    public static void main(String[] args) {
+        // Declare two numbers (operands)
+        int number1 = 15;
+        int number2 = 6;
+
+        // Perform multiplication using the '*' operator
+        int product = number1 * number2;
+
+        // Print the result
+        System.out.println("The product is: " + product); // Output: The product is: 90
+    }
+}
