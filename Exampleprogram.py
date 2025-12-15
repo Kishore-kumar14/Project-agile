1c0d6d031801a9e337fe7846d91a7e472c79ad59
Kishore-kumar14 <kishorekumarsrini2006@gmail.com>
2025-12-15 11:08:29 +0530

    version 2 odd

--- Exampleprogram.py
+++ Exampleprogram.py
@@ -1,5 +1,6 @@
 arr=[]
 arr1=[]
+arr2=[]
 for i in range(0,10):
     arr.append(i)
 print(arr)
@@ -7,3 +8,7 @@ for i in range(0,9):
     if(arr[i]%2==0):
         arr1.append(arr[i])
 print(arr1)
+for i in range(0,9):
+    if(arr[i]%2!=0):
+        arr2.append(arr[i])
+print(arr2)
